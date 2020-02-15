7c0fb4e3ba617487451474ce17615e0bdba1a748
League Student <league@iMac.attlocal.net>
2020-02-15 15:55:01 -0800

    FileRecorder(Ip)
    athahawrystebhgtyhrsirhyetsygdfgrwqte

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,14 @@
 package _01_File_Recorder;
 
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+	FileRecorder g = new FileRecorder();
+	g.gah();	
+	}
+	public void gah() {
+	String message = JOptionPane.showInputDialog("say anything");
+	}
 }
